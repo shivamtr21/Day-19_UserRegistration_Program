d5778ada8eeee848a6d22a2471458aee8040614f
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-10 22:31:43 +0530

    Email Registration

--- UserRegistrationPr.cs
+++ UserRegistrationPr.cs
@@ -9,11 +9,23 @@ namespace UserRegistration
 {
     public class UserRegistrationPr
     {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Email = "^[a-z0-9._+-]+@[a-z|0-9.-]+\\.[a-z.]{2,}$";
 
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
         public bool ValidateLName(string LName)
         {
+
             return Regex.IsMatch(LName, LastName);
+        }
+
+        public bool ValidateMailId(string MailId)
+        {
+            return Regex.IsMatch(MailId, Email);
         }
     }
 }
