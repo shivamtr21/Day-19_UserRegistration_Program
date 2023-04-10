0e8330372297bbf746f1c002d43306c3d4636260
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 01:26:45 +0530

    Phone Registration

--- Program.cs
+++ Program.cs
@@ -18,9 +18,13 @@
             Console.WriteLine("Enter Your Email Id : ");
             string MailId = Console.ReadLine();
 
+            Console.WriteLine("Enter Mobile number : ");
+            string Mobile = Console.ReadLine();
+
             Console.WriteLine(cd.ValidateFName(FName));
             Console.WriteLine(cd.ValidateLName(LName));
             Console.WriteLine(cd.ValidateMailId(MailId));
+            Console.WriteLine(cd.ValidateMobile(Mobile));
         }
     }
 }
