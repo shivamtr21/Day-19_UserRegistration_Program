52e750d062b87520bb5990c437827c3431582807
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-10 20:44:37 +0530

    First Name Registration

---
+++ UserRegistrationPr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class UserRegistrationPr
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
