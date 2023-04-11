8b19d77d75ad5e840b439f2cf77aef9ff2804512
shivamtr21 <shivamtripathi26297@gmail.com>
2023-04-11 20:32:14 +0530

    Password registration

--- Program.cs
+++ Program.cs
@@ -21,10 +21,14 @@
             Console.WriteLine("Enter Mobile number : ");
             string Mobile = Console.ReadLine();
 
+            Console.WriteLine("Enter your assword");
+            string password = Console.ReadLine();
+
             Console.WriteLine(cd.ValidateFName(FName));
             Console.WriteLine(cd.ValidateLName(LName));
             Console.WriteLine(cd.ValidateMailId(MailId));
             Console.WriteLine(cd.ValidateMobile(Mobile));
+            Console.WriteLine(cd.ValidatePassword(password));
         }
     }
 }
